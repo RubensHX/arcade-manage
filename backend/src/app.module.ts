fd94dc569290caccf943e07c408988fcb9b9bba0
Rubens Henrique Pereira <rubensh206@gmail.com>
2022-11-29 20:20:47 -0300

    fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
-import { FirebaseService } from './firebase/firebase.service';
 
 @Module({
   imports: [AuthModule, ConfigModule.forRoot()],
--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -4,20 +4,20 @@ import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly AuthService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Post('register')
   async create(@Body() body: Auth) {
-    this.AuthService.register(body);
+    this.authService.register(body);
   }
 
   @Post('login')
   async login(@Body() body: Auth) {
-    this.AuthService.login(body);
+    this.authService.login(body);
   }
 
   @Post('logout')
   async logout() {
-    this.AuthService.logout();
+    this.authService.logout();
   }
 }
