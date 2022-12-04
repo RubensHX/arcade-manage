d0d4449672b87e690611fdde6d03e8177b27a1c7
Rubens Henrique Pereira <rubensh206@gmail.com>
2022-12-04 17:35:41 -0300

    adsa

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,6 +20,8 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+
+/* App components */
 import SignIn from './pages/SignIn/SignIn';
 import Register from './pages/Register/Register';
 import Start from './pages/Start/Start';
