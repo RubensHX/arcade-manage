bfaabe6b0e7d769f8a67ed701de96fc120981a48
Rubens Henrique Pereira <rubensh206@gmail.com>
2022-12-06 21:14:24 -0300

    feat: melhorias

--- frontend/src/pages/Clients/Clients.tsx
+++ frontend/src/pages/Clients/Clients.tsx
@@ -1,12 +1,12 @@
 import "./Clients.css";
 import { IonPage, IonContent } from "@ionic/react";
-import { useEffect, useId, useMemo, useState, FormEvent } from "react";
+import { useEffect, useMemo, useState, FormEvent } from "react";
 import Topbar from "../../components/Topbar/Topbar";
-import { v4 as uuid } from "uuid";
-import { Trash, WhatsappLogo } from "phosphor-react";
+import { Pencil, Trash, WhatsappLogo } from "phosphor-react";
 import Modal from "react-modal";
 import axios from "axios";
 import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 interface Client {
   id: string;
   name: string;
@@ -19,7 +19,8 @@ export default function Clients() {
   const [clients, setClients] = useState<Client[]>([]);
   const [filteredClients, setFilteredClients] = useState<Client[]>([]);
   const [searchText, setSearchText] = useState("");
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpenNewClient, setIsOpenNewClient] = useState(false);
+  const [isOpenEditClient, setIsOpenEditClient] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
@@ -42,16 +43,25 @@ export default function Clients() {
   }, [searchText, clients]);
 
   function openModal() {
-    setIsOpen(true);
+    setIsOpenNewClient(true);
   }
 
   function closeModal() {
-    setIsOpen(false);
+    setIsOpenNewClient(false);
   }
 
-  function handleClientRemove(client: Client) {
-    axios
-      .delete(`http://localhost:3000/clients/delete/${client.id}`)
+  function openModalEditClient() {
+    setIsOpenEditClient(true);
+  }
+
+  function closeModalEditClient() {
+    setIsOpenEditClient(false);
+  }
+
+  async function handleClientRemove(client: Client) {
+    fetch(`http://localhost:3000/clients/delete/${client.id}`, {
+      method: "DELETE",
+    })
       .then(() => {
         toast.success("Cliente removido com sucesso!");
         setClients(clients.filter((c) => c.id !== client.id));
@@ -59,6 +69,7 @@ export default function Clients() {
       .catch(() => {
         toast.error("Não foi possível remover o cliente!");
       });
+      
   }
 
   const handleNewClient = (event: FormEvent) => {
@@ -75,6 +86,29 @@ export default function Clients() {
     closeModal();
     toast.success("Cliente cadastrado com sucesso!");
   };
+
+  const handleClientEdit = (client: Client) => {
+    openModalEditClient();
+    setName(client.name);
+    setEmail(client.email);
+    setPhone(client.phone);
+    setAddress(client.address);
+  };
+
+  const saveClientEdit = (event: FormEvent) => {
+    event.preventDefault();
+    axios(`http://localhost:3000/clients/update/`, {
+      method: "PUT",
+      data: {
+        name: name,
+        email: email,
+        phone: phone,
+        address: address,
+      },
+    });
+    closeModalEditClient();
+    toast.success("Cliente editado com sucesso!");
+  }
 
   return (
     <IonPage>
@@ -116,6 +150,22 @@ export default function Clients() {
                             </a>
                           </p>
                           <p>{client.address}</p>
+                          <div className="separateDiv">
+                            <button
+                              className="trash-button"
+                              onClick={() => handleClientRemove(client)}
+                            >
+                              <Trash size={16} color="red" /> Excluir cliente
+                            </button>
+                          </div>
+                          <div className="separateDiv">
+                            <button
+                              className="trash-button"
+                              onClick={() => handleClientEdit(client)}
+                            >
+                              <Pencil size={16} color="blue" /> Editar cliente
+                            </button>
+                          </div>
                         </div>
                       </li>
                     </>
@@ -144,10 +194,21 @@ export default function Clients() {
                             </a>
                           </p>
                           <p>{client.address}</p>
-                          <div className="clientRemove">
-                            <button onClick={() => handleClientRemove(client)}>
-                                <Trash size={16} color="red" /> Excluir cliente
-                            </button>
+                          <div className="separateDiv">
+                            <div className="buttonGrid">
+                              <button
+                                className="action-button-danger"
+                                onClick={() => handleClientRemove(client)}
+                              >
+                                <Trash size={16} color="white" />
+                              </button>
+                              <button
+                                className="action-button"
+                                onClick={() => handleClientEdit(client)}
+                              >
+                                <Pencil size={16} color="white" />
+                              </button>
+                            </div>
                           </div>
                         </div>
                       </li>
@@ -167,7 +228,7 @@ export default function Clients() {
             </button>
           </div>
           <Modal
-            isOpen={isOpen}
+            isOpen={isOpenNewClient}
             onRequestClose={closeModal}
             style={{
               overlay: {
@@ -232,6 +293,75 @@ export default function Clients() {
             </form>
           </Modal>
         </footer>
+        <Modal
+          isOpen={isOpenEditClient}
+          onRequestClose={closeModalEditClient}
+          style={{
+            overlay: {
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+            },
+            content: {
+              width: "fit-content",
+              height: "fit-content",
+              backgroundColor: "#fff",
+              border: "none",
+              overflowY: "hidden",
+              color: "#000",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              top: "50%",
+              left: "50%",
+              right: "auto",
+              bottom: "auto",
+              marginRight: "-50%",
+              transform: "translate(-50%, -50%)",
+            },
+          }}
+          contentLabel="Cadastro de cliente"
+        >
+          <h1>Editar cliente</h1>
+          <form onSubmit={saveClientEdit} className="createClientForm">
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={name}
+              placeholder="Nome"
+              onChange={(e) => setName(e.target.value)}
+              autoComplete="off"
+            />
+            <input
+              type="text"
+              name="email"
+              id="email"
+              value={email}
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              autoComplete="off"
+            />
+            <input
+              type="text"
+              name="phone"
+              id="phone"
+              value={phone}
+              placeholder="Telefone"
+              onChange={(e) => setPhone(e.target.value)}
+              autoComplete="off"
+            />
+            <input
+              type="text"
+              name="address"
+              id="address"
+              value={address}
+              placeholder="Endereço"
+              onChange={(e) => setAddress(e.target.value)}
+              autoComplete="off"
+            />
+            <button type="submit">Editar</button>
+          </form>
+        </Modal>
         <ToastContainer />
       </IonContent>
     </IonPage>
